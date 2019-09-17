f66b72a229f734e82362f93a0f4d9b7a727d21a0
Zain Anwar Rajani <zainrajani2207@gmail.com>
2019-09-17 09:18:46 -0400

    Add files via upload

---
+++ Bode_plot_example.m
@@ -0,0 +1,26 @@
+%---------------Frequency Response Analysis--------
+
+%----------------BODE PLOT-------------------------
+
+
+clc;
+clear all;
+close all;
+s=tf('s');
+GH=10/(s*(1+0.4*s)*(1+0.1*s));
+%num=10*[0 0 0];
+%den= conv(conv([1 0],[0.4 1]),[0.1 1]);
+%=tf(num,den);
+bode(GH)                      %Main function in the code
+grid on
+[Gm,Pm,wcp,wcg]=margin(GH);   %To find the margin values 
+disp('Gain Margin is') 
+disp(Gm)
+fprintf('Phase Margin is %f \n',Pm)
+disp('Phase crossover frequency')
+disp(wcp)
+disp('Gain Crossover Frequency') 
+disp(wcg)
+margin(GH)
+grid on
+%--------------END OF THE CODE----------------------
---
+++ Bode_plot_expo_zpk.m
@@ -0,0 +1,38 @@
+%-----------------FREQUENCY DOMAIN ANALYSIS--------------
+
+%----------------BODE PLOT (EXPONENTIAL)-----------------
+
+clc;
+clear all;
+close all;
+s=tf('s');
+G=1*(exp(-0.2*s))/(s*(1+s)*(1+0.2*s));
+bode(G)
+grid on
+margin(G)
+[Gm, pm, wcp, wcg]=margin(G);
+disp('Gain margin is')
+disp(Gm)
+disp('Phase Margin is')
+disp(pm)
+disp('Phase Crossover Frequency')
+disp(wcp)
+disp('Gain CrossOver frequency')
+disp(wcg)
+
+%---------------------END-------------------------------%
+
+%%
+%----------------BODE PLOT USING zpk--------------------
+
+clc;
+clear all;
+close all;
+G=zpk([-4],[1 2],10);
+figure
+margin(G)
+grid on;
+disp('Transfer function is');
+G
+
+%------------------------END---------------------------
