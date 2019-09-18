97b0d50deca511925321ab8a6f61aed5d0ed7e4e
Zain Anwar Rajani <zainrajani2207@gmail.com>
2019-09-17 22:49:24 -0400

    Add files via upload

---
+++ Finding_response_for_Various_inputs.m
@@ -0,0 +1,28 @@
+%Finding step, ramp and parabolic input response for a closed loop
+% G(s)= 3/s(s+4)
+
+clc;
+clear all;
+close all;
+g1=3;
+g2=conv([1 0],[1,4]);
+g=tf(g1,g2);
+h=1;
+c=feedback(g,1);
+step(c)
+t=0:0.001:10;
+u=t;
+[y,x]=lsim(c,u,t);
+figure; plot(t,y,t,u)
+xlabel('Time in seconds')
+ylabel('Amplitude')
+title('Closed loop response for ramp input');
+legend('Actual Signal','Desired Signal')
+u1=0.5*t.^2;
+[y1,x1]=lsim(c,u1,t); %plot the values depending on the history of u1 and t for c
+figure;
+plot(t,y1,u1);
+xlabel('Time in seconds')
+ylabel('Amplitude')
+title('Closed loop response for parabolic input');
+legend('Actual Signal','Desired Signal')
