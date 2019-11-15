c51569a3eeed5f20729273ab3c9de1da7f06f12e
Zain Rajani <zainrajani2207@gmail.com>
2019-11-14 23:22:13 -0500

    Update Second_Order_System_for_different_values_of_Zeta.m

--- Second_Order_System_for_different_values_of_Zeta.m
+++ Second_Order_System_for_different_values_of_Zeta.m
@@ -6,8 +6,9 @@ clc;
 clear all;
 close all;
 %% General Parameters for a second order system
-w=10;
-for l=[0 0.2 0.4 1.0 1.5];
+% Second order system is descibed using the formulae:
+w=10;  % Omega value 
+for l=[0 0.2 0.4 1.0 1.5];  % for loop usage and computing the numerator and denomintor values
 num=[w.^2];
 den=[1 2*l*w w.^2];
 t=0:0.01:2;
