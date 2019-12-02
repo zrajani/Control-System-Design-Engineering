3592b91490d1b4fd25e0a8557cece92853aed774
zrajani <zainrajani2207@gmail.com>
2019-12-02 16:14:14 -0500

    Nyquist Plot

---
+++ Nyquist_plot.m
@@ -0,0 +1,16 @@
+%------------------FREQUENCY DOMAIN ANALYSIS----------------
+
+%-----------------NYQUIST PLOT------------------------------
+
+clc;
+clear all;
+close all;
+s=tf('s');
+K=240;               %___Assume the value of gain and try___________
+%GH=(K*(s-6))/((s+2)^2)
+GH=K/(s*(s+2)*(s+10))
+nyquist(GH,'k')
+axis([-4 0.5 -2 2]);
+%grid on;
+
+%--------------END-------------------------------------------
